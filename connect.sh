7f645d15d37ead49c12f28aa1b8c1e29155f4550
Uday Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2018-06-27 12:25:39 +0100

    Added feature to connect mysql

--- connect.sh
+++ connect.sh
@@ -4,11 +4,13 @@ BASE_NAME=`basename $CUR_DIR |sed 's/_//g'`
 container=${1}
 if [ "$1" = "" ]; then
     container=web
+elif [ "$1" == "mysql" ]; then
+   container=db
 fi
 
 which_container=${BASE_NAME}_${container}_1
 
-if [ "$1" = "db" ]; then
+if [ "$1" = "db"  ] || [ "$1" = "mysql"  ] ; then
     docker exec -it $which_container mysql -u root -proot
 else
     docker exec -it $which_container /bin/bash
